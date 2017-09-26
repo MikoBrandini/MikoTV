7360e392ba4d366de41bec4c54d03cba20b5e5e1
Miko Brandini <MikoBrandini@gmail.com>
2017-09-26 18:40:15 -0400

    Still under construction. Mobile first (and for now, only)

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 import VideoDetail from './components/video_detail';
+import keys from './keys.js';
 
-const API_KEY = 'AIzaSyAl8Sl79XqYjq3mjxJZDRX4iC_2wz4EsVE';
-
+const API_KEY = keys.youtubeApiKey;
+console.log(API_KEY)
 
   class App extends Component {
     constructor(props){
---
+++ src/keys.js
@@ -0,0 +1,5 @@
+const keys = {
+  youtubeApiKey: 'AIzaSyAl8Sl79XqYjq3mjxJZDRX4iC_2wz4EsVE'
+};
+
+export default keys;
